4dfcf808b8d7a4871afa8eacea7b2a71dabf4ecc
Sekthorn <rtyuqwe43@gmail.com>
2026-01-16 08:03:38 +0700

    tp5

---
+++ laravel/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(CategoryController::class)->prefix('categories')->group(function () {
+    Route::get('/', 'categories'); // Get all categories
+    Route::post('/', 'createCategory');
+    Route::get('/{categoryId}','getCategory');
+    Route::patch('/{categoryId}','updateCategory');
+    Route::delete('/{categoryId}','deleteCategory');
+});
